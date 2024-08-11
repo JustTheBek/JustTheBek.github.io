9a007392f690dfac0c71d50e87047000ea95250d
JustTheBek <zoltan.bek@proton.me>
2024-08-11 17:16:58 +0200

    Added initial version of real Blog data.

---
+++ assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,26 @@
+var store = [{
+        "title": "Értelmet kell keresni az életnek.",
+        "excerpt":"Gondolatok a háborúról a Szakasz című film kapcsán. Sajnos égetően aktuális téma a háború, így hát valószínűleg nem ez lesz az utolsó írásom a témában. A megrázó valóság feldolgozását segítendő, a közelmúltban megnéztem Oliver Stone 1986-os klasszikusát. Mélyen felkavart. A filmről röviden Önkéntesnek jelentkezik a vietnami háborúba az ifjú Chris Taylor. Otthagyja az egyetemet, hogy Vietnamba menjen harcolni egy olyan ügyért, amelyről azt hitte, hazáját és Vietnam felszabadítását szolgálja. A repülőről leszállva azonban máris egy másik világban találja magát. Szakaszát két sokat tapasztalt katona vezeti: Barnes őrmester, akinek sebhelyek szántotta arca híven tükrözi lelke romlottságát, és Elias őrmester, a morál és a tisztesség élő szobra, aki másokkal ellentétben még mindig az első sorban harcol. A két vezető között kibontakozó éles konfliktusban Chris számára hamarosan kirajzolódik a vietnami háború igazi, értelmetlen erőszaktól fűtött képe, amelyben a katonák nem az ellenséggel harcolnak, hanem saját magukkal. Személyes élmények Hamarosan… ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/home/movies/2024-08-08-platoon/"
+      },{
+        "title": "Szólásszabadság, vagy erkölcsi fertő?",
+        "excerpt":"Gondolatok a szólás- és sajtószabadságról a Larry Flint, a provokátor című film kapcsán. A filmről röviden A hetvenes évek elején a vállalkozó kedvű Larry Flynt, aki addig a szesz- és sztriptíziparban próbálgatta oroszlánkörmeit, úgy dönt, elérkezett az idő, hogy megtörje a Playboy egyeduralmát. Az általa kitalált szexújság, a Hustler magazin nem kendőzi a valóságot: mindent megmutat, amit a többi lap eddig szemérmesen eltakart. A pornómagazin hamarosan közfeltűnést kelt és közbotrányt okoz. Miközben Flynt, akit százezrek istenítenek és milliók gyűlölnek, elszántan küzd a totális sajtószabadságért, ellenfelei brutális eszközökhöz nyúlnak ellene. Személyes élmények Hamarosan… ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/home/movies/2024-08-09-larry-flint/"
+      },{
+        "title": "Miért is gyűlöljük a pedofilokat?",
+        "excerpt":"Gondolatok a pedofíliáról és kisközösségek toxikus működéséről a Vadászat című film kapcsán. A filmről röviden A nemrég elvált Lucas egy vidám baráti társaság megbecsült tagja, gondoskodó apa, megbízható munkatárs. Egy óvodában dolgozik asszisztensként, szereti a munkáját, és a gyerekek, kollégák is kedvelik őt. Egyik kolléganőjével randizni kezd, és úgy tűnik, magánélete is mederbe kerül. A harmonikus hétköznapok azonban egy pillanat alatt omlanak össze egy apró füllentés hatására: az egyik kislány, legjobb barátja gyermeke az óvodában azt mondja, hogy Lucas tett vele valamit. A gyermeki hazugság visszafordíthatatlan károkat okozva szabadul el, a magára maradó, védtelen Lucas pedig értetlenül áll az események előtt. Személyes élmények Hamarosan… ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/home/movies/2024-08-10-jagten/"
+      },{
+        "title": "Mindnyájan drogosok vagyunk.",
+        "excerpt":"Gondolatok a függőségekről a Rekviem egy álomért című film kapcsán. A filmről röviden Harry Goldfarb egyetlen pénzszerzési lehetősége, hogy rendszeresen eladja anyja tévéjét, akinek a TV nézés élete egyetlen értelme és legfőbb szórakozása. A pénz heroinra kell, a TV pedig azért jó üzlet, mert Harry anyja úgyis mindig visszavásárolja. Harry édesanyja, Sara egy más világról álmodik. Mindent megtesz azért, hogy bekerülhessen kedvenc TV show-jába. Személyes élmények Hamarosan… ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/home/movies/2024-08-11-requiem/"
+      },{
+        "title": "Értelmet kell keresni az életnek - új poszt a Filmvilágon",
+        "excerpt":"A poszt a Filmvilág rovatban jelent meg, az alábbi linkre kattintva olvasható: http://127.0.0.1:4000/home/movies/2024-08-08-platoon/ actions:   label: “Ugrás a teljes posztra”icon: githuburl: “http://127.0.0.1:4000/home/movies/2024-08-08-platoon/”","categories": [],
+        "tags": ["A szakasz","Platoon","háború","vietnámi háború","Oliver Stone"],
+        "url": "http://localhost:4000/home/2024/08/08/platoon-post.html"
+      }]
